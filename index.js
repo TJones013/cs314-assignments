265eb677454c8e55f006c4a87d3c038e6b2b3dae
TJones013 <taylorcj013@gmail.com>
2018-04-25 20:23:42 -0700

    another push

--- index.js
+++ index.js
@@ -1,43 +1,78 @@
 $(document).ready(function(){
+    let flag1;
+    let flag2;
     $.get("https://jsonplaceholder.typicode.com/users").then(function(data){
         $.each(data, function(i, item){
             $("#div_main").append('<div id="div' + i + '">'+
             '<p>' + 'Name: ' + item.name + '</p>' +
             '<p>' + 'Email: ' + item.email + '</p>'+
             '<p>' + 'Company: ' + item.company.name + '</p>'+
-            '<button id="button' + i + '">' + 'Click test' + '</button>' +
+            '<button id="button' + i + '">' + 'Todo' + '</button>' +
+            '<button id="a_button' + i + '">' + 'Album' + '</button>' +
             '</div>');
-            console.log(item.id);
-        });
-        $("#button").click(function(){
-            console.log('test');
-        });
-    });
-});
-
-/*
-let preload_todo = {};
-
-$(document).ready(function(){
-    $("#button0").one('click', function(){
-        $.get("https://jsonplaceholder.typicode.com/todos").then(function(data){
-            $.each(data, function(i, item){
-                console.log(item.id);
+            $("#button" + i).one('click', function(){
+                buttonContent(i);
+                $("#todo_" + i).show();
+                $("#album_" + i).hide();
             });
-        });
-    });
-});
-*/
+            if(!flag1){
+                $("#button" + i).click(function(){
+                    $("#todo_" + i).toggle();
+                    $("#album_" + i).hide();
+                });
+            }
 
-function buttonContent(user){
-    let flag = true;
-    if(flag){
-        $.get("https://jsonplaceholder.typicode.com/todos").then(function(data){
-        $.each(data, function(i, item){
-            if(user == item.userId){
-                console.log(item.id);
+            $("#a_button" + i).one('click', function(){
+                albumContent(i);
+                $("#album_" + i).show();
+                $("#todo_" + i).hide();
+            });
+            if(!flag2){
+                $("#a_button" + i).click(function(){
+                    $("#album_" + i).toggle();
+                    $("#todo_" + i).hide();
+                });
             }
         });
     });
+});
+
+function buttonContent(user){
+    //console.log(flag);
+    flag = true;
+    if(flag){
+        flag = false;
+        $("#div" + user).append('<div id="todo_' + user + '">');
+        $.get("https://jsonplaceholder.typicode.com/todos").then(function(data){
+        $.each(data, function(i, item){
+            //console.log(user,item.userId);
+            if((user + 1) == item.userId){
+                //console.log(flag);
+                $("#todo_" + user).append('<p>' + 'Todo: ' + item.title + '</p>');
+                if(item.completed){
+                    $("#todo_" + user).append('<i class="fas fa-check-square"></i>');
+                }
+                else{
+                    $("#todo_" + user).append('<i class="fas fa-times-circle"></i>');
+                }
+            }
+            $("#todo_" + user).show();
+        });
+    });
     }
+    return false;
+}
+
+function albumContent(user){
+    $("#div" + user).append('<div id="album_' + user + '">');
+        $.get("https://jsonplaceholder.typicode.com/albums").then(function(data){
+        $.each(data, function(i, item){
+            //console.log(user,item.userId);
+            if((user + 1) == item.userId){
+                //console.log(flag);
+                $("#album_" + user).append('<p>' + 'Album: ' + item.title + '</p>');
+            }
+            $("#album_" + user).show();
+        });
+    });
 }
